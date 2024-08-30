573695bb98dfe582b3612f2c55c1612bff3f1e84
auntehhmakazz <loveylovey2007@gmail.com>
2024-08-30 12:59:40 -1000

    Create Cube_Calculator.java

---
+++ Cube_Calculator.java
@@ -0,0 +1,34 @@
+public class Intro_Cube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * this class is about finding the side length, surface area and the volume of a cube
+		 */
+		
+		//This declares my side length
+		double x = 4.0;
+		//this declares my surface area
+		double s = 6*x*x;
+		//this declares my volume
+		double v = x*x*x;
+		
+		//printed out my side length s
+		System.out.print("side length is ");
+		System.out.println((int)x);
+		//printed out my volume 
+		System.out.print("the volume of the cube is ");
+		System.out.print((int)v);
+		//printed out my surface area 
+		System.out.print("the surface area of the cube is ");
+		System.out.print((int)s);
+		
+		
+	
+		
+		
+		
+	}
+
+}
