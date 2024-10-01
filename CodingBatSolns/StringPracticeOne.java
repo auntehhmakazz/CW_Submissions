161b02ac7a1a26ed47cba2b8f3a55bc3b205f86f
auntehhmakazz <loveylovey2007@gmail.com>
2024-10-01 08:54:51 -1000

    Update StringPracticeOne.java

--- CodingBatSolns/StringPracticeOne.java
+++ CodingBatSolns/StringPracticeOne.java
@@ -10,3 +10,9 @@ public String makeAbba(String a, String b) {
   return a+b+b+a;
 }
 
+//thsi is practice 3 - makeTags
+public String makeTags(String tag, String word){
+  
+  return "<"+tag+">"+word+"</"+tag+">";
+
+}
