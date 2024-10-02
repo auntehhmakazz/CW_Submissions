de008f7520eee5d4154a4f14e4eba9499438714c
auntehhmakazz <loveylovey2007@gmail.com>
2024-10-02 09:16:38 -1000

    Update Cube_Calculator.java

--- Cube_Calculator.java
+++ Cube_Calculator.java
@@ -1,28 +1,28 @@
+/*
+ * this class is about finding the side length, surface area and the volume of a cube
+ */
 public class Intro_Cube {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
+// TODO Auto-generated method stub
 
-		/*
-		 * this class is about finding the side length, surface area and the volume of a cube
-		 */
+			
+//This function declares my side length of a cube
+	double x = 4.0;
+//this function declares my surface area of a cube
+	double s = 6*x*x;
+//this function declares my volume of a cube
+	double v = x*x*x;
 		
-		//This declares my side length
-		double x = 4.0;
-		//this declares my surface area
-		double s = 6*x*x;
-		//this declares my volume
-		double v = x*x*x;
-		
-		//printed out my side length s
-		System.out.print("side length is ");
-		System.out.println((int)x);
-		//printed out my volume 
-		System.out.print("the volume of the cube is ");
-		System.out.print((int)v);
-		//printed out my surface area 
-		System.out.print("the surface area of the cube is ");
-		System.out.print((int)s);
+//this will print out my side length of the cube which would be 4
+	System.out.print("side length is ");
+	System.out.println((int)x);
+//this will print out my volume of the cube which should be 64
+	System.out.print("the volume of the cube is ");
+	System.out.println((int)v);
+//this will print out my surface area whcih should eb 96
+	System.out.print("the surface area of the cube is ");
+	System.out.print((int)s);
 		
 		
 	
