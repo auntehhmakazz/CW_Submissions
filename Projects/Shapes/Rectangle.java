eb543501e948e7dd5da73731d3bd6d1235e9ec33
auntehhmakazz <loveylovey2007@gmail.com>
2024-10-02 08:47:51 -1000

    Update Rectangle.java

--- Projects/Shapes/Rectangle.java
+++ Projects/Shapes/Rectangle.java
@@ -4,37 +4,37 @@
 public class Rectangle {
 
 //DATA ATTRIBUTES
-  private double length;
-  private double width;
+	private double length;
+	private double width;
 
 //CONSTRUCTOR
-  public Rectangle(double l, double w)
-    {
-    	length = l;
-    	width = w;
-    }
+		public Rectangle(double l, double w)
+		{
+			length = l;
+			width = w;
+		}
 
 
 //FUNCTIONALITY
-  /**
-   * 
-   *this method will return the perimeter of a rectangle
-   * @return the perimeter
-   */
-    public double getPerimeter()
-      {
-	      return 2*(length+width);
-      }
-
-  /**
-   * 
-   * this method will return the area of a area
-   * @return the area
-   */
-    public double getArea()
-      {
-	      return width*length;
-      }
+		/**
+		 * 
+		 *this method will return the perimeter of a rectangle
+		 * @return the perimeter
+		 */
+		public double getPerimeter()
+		{
+			return 2*(length+width);
+		}
+		
+		/**
+		 * 
+		 * this method will return the area of a area
+		 * @return the area
+		 */
+		public double getArea()
+		{
+			return width*length;
+		}
 
 //public Rectangle(int l, int w)
 //{
@@ -51,6 +51,16 @@ public class Rectangle {
 
 
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
