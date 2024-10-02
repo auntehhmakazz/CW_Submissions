0612b34094fd4fb1b37e39b2cd16959c34511949
auntehhmakazz <loveylovey2007@gmail.com>
2024-10-02 08:44:37 -1000

    Update Cube.java

--- Projects/Shapes/Cube.java
+++ Projects/Shapes/Cube.java
@@ -4,35 +4,36 @@
 public class Cube {
 
 //Data Attributes
-  private double length;
-  private double width;
+		private double length;
+		private double width;
 
 
 //constructor
-  public Cube(double l, double w)
-    {
-    	length = l;
-    	width = w;
-    }
-	
+		public Cube(double l, double w)
+		{
+			length = l;
+			width = w;
+		}
+			
 //functionality
-  /**
-   * this method will return the perimeter of the cube
-   * @return the perimeter
-   */
-  public double getPerimeter()
-    {
-    	return 2*length+width;
-    }
-  /**
-   * this method will return the area of the cube
-   * @return the area
-   */
-  public double getArea()
-    {
-    	return length*width;
-    }
 
+		/**
+		 * this method will return the perimeter of the cube
+		 * @return the perimeter
+		 */
+		public double getPerimeter()
+		{
+			return 2*length+width;
+		}
+		/**
+		 * this method will return the area of the cube
+		 * @return the area
+		 */
+		public double getArea()
+		{
+			return length*width;
+		}
+		
 
 
 
