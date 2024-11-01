a5f7975cf776435704e5439c0a3a2122a17278ca
auntehhmakazz <loveylovey2007@gmail.com>
2024-11-01 13:41:23 -1000

    Create LogicQuiz_Corrections.java

---
+++ LogicQuiz_Corrections.java
@@ -0,0 +1,33 @@
+/*
+ * this is my quiz corrections for my logic quiz which was 
+ * taken on October 28, 2024
+ */
+
+public class LogicQuiz_Corrections {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print(findIfIsLeapYear(300));
+		
+	}
+	
+	public static boolean findIfIsLeapYear(int year)
+	{
+		if ( year % 400 ==0)
+		{
+			return true;
+		}
+		else if (year % 400 != 0 || year % 100 == 0)
+		{
+			return false;
+		}
+
+		return false;
+	}
+
+
+	
+	
+	
+}
