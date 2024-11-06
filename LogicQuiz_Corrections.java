96d7e971284cf8f21aecf8b12102279d29b4f2d9
auntehhmakazz <loveylovey2007@gmail.com>
2024-11-06 08:10:42 -1000

    Update LogicQuiz_Corrections.java

--- LogicQuiz_Corrections.java
+++ LogicQuiz_Corrections.java
@@ -8,29 +8,31 @@ public class LogicQuiz_Corrections {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print(findIfIsLeapYear(300));
+		System.out.print(findIfIsLeapYear(-4));
 		
 	}
-
-	// this is the method
+	
 	public static boolean findIfIsLeapYear(int year)
 	{
-		// if the year is divisble by 400 it will return true 
-		if ( year % 400 ==0)
+		if (year % 400 ==0 )
 		{
 			return true;
 		}
-		// if it isn't divisible by 400 or 100 it will return false
-		else if (year % 400 != 0 || year % 100 == 0)
+		else if (year % 100 == 0)
 		{
 			return false;
 		}
-
-		return false;
+		else if (year % 4 == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 
 	
 	
 	
-}
